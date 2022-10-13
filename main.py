3640c33ba84b9c8e4b659cfdeb12166ee613316c
MrEgyptian <Me@MrEgyptian.com>
2022-10-14 00:05:50 +0200

    .

--- main.py
+++ main.py
@@ -6,11 +6,18 @@
 # Contact : Me@MrEgyptian.com
 ########################################################################
 #
-
-
-import requests
+from pygments import highlight, lexers, formatters
+import requests,json
+import colorama
 import base64
 from requests.auth import HTTPBasicAuth
+from configparser import ConfigParser as cp
+parser=cp()
+red=colorama.Fore.RED
+green=colorama.Fore.GREEN
+blue=colorama.Fore.BLUE
+yellow=colorama.Fore.YELLOW
+
 class invalidCreds(Exception):
  '''Invalid Username or Password'''
 class CPanelAPI:
@@ -37,7 +44,7 @@ class CPanelAPI:
   content=f.read()
   f.close()
   return name,content
- def upload(self,path,rootDir="public_html",dir="ze"):
+ def upload(self,path,rootDir="public_html",dir=""):
   fileName,fileContent=self.getFile(path)
   self.uploadFile(fileName,fileContent,rootDir=rootDir,dir=dir)
   pass
@@ -53,7 +60,14 @@ class CPanelAPI:
   }
 
   r=self.session.post(self.url,auth=self.auth,data=params,files=f)
-  print(r.status_code,r.json())
+  status=r.status_code
+  obj=r.json()['cpanelresult']['data']
+  json_str=json.dumps(obj,indent=4)
+  hi= highlight(json_str,
+    lexers.JsonLexer(),
+    formatters.TerminalFormatter()
+    )
+  print(green,r.status_code,hi)
   pass
 def rmComment(stringo):
  stringo=str(stringo).strip('\n')
@@ -70,31 +84,47 @@ def readList(list_name):
  lines=list(lines)
  return lines
 def login(host,port,user,password):
+   global green,blue,red,yellow
    try:
-    print(f"Logging in as {user}")
+    print(f"{yellow}Logging in as {blue}{user}")
     api=CPanelAPI(hostname=host,port=int(port),username=user,password=password)
-    print(f'Logged in Successfully')
+    print(f'{green}Logged in Successfully')
     return api
    except Exception as e:
-    print('Error While Logging in ',e)
+    print('{red} Error While Logging in ',e)
    return False
+def get_file_input(disp_text,error_text='invalid file name : '):
+ global red
+ print(disp_text,end='')
+ file=input()
+ try:
+  x=open(file)
+  x.close()
+  return file
+ except:
+  print(red,error_text,file)
+  return get_file_input(disp_text,error_text=error_text)
 if __name__=='__main__':
  # Some Testing creds
  # https://jossjt1.websitewelcome.com:2083/
  # fortest8471
  # ForTest#@ForTest#@
  #
- listFile='list.txt'
- cpanel_list=readList(listFile)
- print(f'Reading list [{listFile}]')
+ colorama.init()
+ listFile=get_file_input(f'{green}CPanels >{yellow} ')
+ uploadFile=get_file_input(f'{green}UPLOAD FILE >{yellow} ')
+ cpanel_list=readList(listFile) 
+ print(f'Reading list {blue}[{green}{listFile}{blue}]')
  for item in cpanel_list:
   try:
    host,port,user,password=item
-   print(f"Connecting to https://{host}:{port}")
+   print(f"{yellow}Connecting to {blue}https://{host}:{port}")
    api=login(*item)
    if(api!=False):
-    api.upload('test.html')
+    print(f"{yellow}Uploading {blue}{uploadFile}")
+    api.upload(uploadFile)
   except Exception as e:
    print(f'invalid syntax in {":".join(item)}',e)
   #api=CPanelAPI()
  #api.uploadFile('kaka.html',"<center>PePe</center>")
+
